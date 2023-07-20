6258c269f7dbf1b32a6c5998c2b4fa7e1883572c
Vik154 <Smorodin_1209@mail.ru>
2023-07-21 06:32:07 +0700

    Реализация интерфейса ITextFieldsRepository для работы с текстовыми полями

--- 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Domain/Repositories/EntityFramework/EFTextFieldsRepository.cs
+++ 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Domain/Repositories/EntityFramework/EFTextFieldsRepository.cs
@@ -1,4 +1,42 @@
-﻿namespace WebAppCoreV3.Domain.Repositories.EntityFramework {
-    public class EFTextFieldsRepository {
+﻿// Реализация интерфейса ITextFieldsRepository для работы с текстовыми полями
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System;
+using WebAppCoreV3.Domain.Entities;
+using WebAppCoreV3.Domain.Repositories.Abstract;
+
+namespace WebAppCoreV3.Domain.Repositories.EntityFramework {
+
+    public class EFTextFieldsRepository : ITextFieldsRepository {
+        private readonly AppDbContext context;
+        public EFTextFieldsRepository(AppDbContext context) {
+            this.context = context;
+        }
+
+        public IQueryable<TextField> GetTextFields() {
+            return context.TextFields;
+        }
+
+        public TextField GetTextFieldById(Guid id) {
+            return context.TextFields.FirstOrDefault(x => x.Id == id);
+        }
+
+        public TextField GetTextFieldByCodeWord(string codeWord) {
+            return context.TextFields.FirstOrDefault(x => x.CodeWord == codeWord);
+        }
+
+        public void SaveTextField(TextField entity) {
+            if (entity.Id == default)
+                context.Entry(entity).State = EntityState.Added;
+            else
+                context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void DeleteTextField(Guid id) {
+            context.TextFields.Remove(new TextField() { Id = id });
+            context.SaveChanges();
+        }
     }
+
 }
