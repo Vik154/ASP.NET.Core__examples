f0addf3533c68f227808b0e2be83a48030d40afc
Vik154 <Smorodin_1209@mail.ru>
2023-07-21 06:37:47 +0700

    Реализация интерфейса IServiceItemsRepository (управление услугами на сайте)

--- 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Domain/Repositories/EntityFramework/EFServiceItemsRepository.cs
+++ 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Domain/Repositories/EntityFramework/EFServiceItemsRepository.cs
@@ -1,4 +1,37 @@
-﻿namespace WebAppCoreV3.Domain.Repositories.EntityFramework {
-    public class EFServiceItemsRepository {
+﻿// Реализация интерфейса IServiceItemsRepository (управление услугами на сайте)
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System;
+using WebAppCoreV3.Domain.Entities;
+using WebAppCoreV3.Domain.Repositories.Abstract;
+
+namespace WebAppCoreV3.Domain.Repositories.EntityFramework {
+
+    public class EFServiceItemsRepository : IServiceItemsRepository {
+        private readonly AppDbContext context;
+        public EFServiceItemsRepository(AppDbContext context) {
+            this.context = context;
+        }
+
+        public IQueryable<ServiceItem> GetServiceItems() {
+            return context.ServiceItems;
+        }
+
+        public ServiceItem GetServiceItemById(Guid id) {
+            return context.ServiceItems.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void SaveServiceItem(ServiceItem entity) {
+            if (entity.Id == default)
+                context.Entry(entity).State = EntityState.Added;
+            else
+                context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void DeleteServiceItem(Guid id) {
+            context.ServiceItems.Remove(new ServiceItem() { Id = id });
+            context.SaveChanges();
+        }
     }
 }
