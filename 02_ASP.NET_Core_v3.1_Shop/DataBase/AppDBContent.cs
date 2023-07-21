34bcc02d4bf7abcb3b5a9925cbcf7716bfdf271f
Vik154 <Smorodin_1209@mail.ru>
2023-07-22 06:08:50 +0700

    Класс для работы с базой данных

---
+++ 02_ASP.NET_Core_v3.1_Shop/DataBase/AppDBContent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shop.DataBase {
+    
+    public class AppDBContent : DbContext {
+        public AppDBContent(DbContextOptions<AppDBContent> options) 
+            : base(options) { }            
+    }
+}
