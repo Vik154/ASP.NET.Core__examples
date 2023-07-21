4c4455dd07ecbbf8f6353a704614bf8d7a7aab62
Vik154 <Smorodin_1209@mail.ru>
2023-07-21 10:17:25 +0700

    Редактирование ссылок

---
+++ 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Areas/Admin/Controllers/TextFieldsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppCoreV3.Areas.Admin.Controllers {
+    public class TextFieldsController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
