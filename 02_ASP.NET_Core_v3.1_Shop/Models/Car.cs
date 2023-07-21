d684b7f00bcfd0810b3fbbbba92fe31a353e1d5e
Vik154 <Smorodin_1209@mail.ru>
2023-07-22 03:02:47 +0700

    Реализация класса отвечающего за товары

--- 02_ASP.NET_Core_v3.1_Shop/Models/Car.cs
+++ 02_ASP.NET_Core_v3.1_Shop/Models/Car.cs
@@ -1,15 +1,18 @@
-﻿namespace Shop.Data.Model;
+﻿// Класс отвечающий за товары
 
-public class Car {
+namespace Shop.Models {
 
-    public int    Id { get; set; }
-    public string Name { get; set; }
-    public string shortDesc          { get; set; }
-    public string longDesc           { get; set; }
-    public string img                { get; set; }
-    public ushort price              { get; set; }
-    public bool   isFavourite        { get; set; }
-    public bool   available          { get; set; }
-    public int    categoryID         { get; set; }
-    public virtual Category Category { get; set; }
+    public class Car {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string shortDesc { get; set; }
+        public string longDesc { get; set; }
+        public string img { get; set; }
+        public ushort price { get; set; }
+        public bool isFavourite { get; set; }
+        public bool available { get; set; }
+        public int categoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
 }
