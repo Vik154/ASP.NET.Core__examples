5df5d1eb842027e5e8ce31ee728f1ed85334560b
Vik154 <Smorodin_1209@mail.ru>
2023-07-21 07:00:25 +0700

    Класс-помощник, точка входа для DataBase контекста

---
+++ 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Domain/DataManager.cs
@@ -0,0 +1,16 @@
+﻿// Класс-помощник, точка входа для DataBase контекста
+using WebAppCoreV3.Domain.Repositories.Abstract;
+
+namespace WebAppCoreV3.Domain {
+
+    public class DataManager {
+    
+        public ITextFieldsRepository TextFields { get; set; }
+        public IServiceItemsRepository ServiceItems { get; set; }
+
+        public DataManager(ITextFieldsRepository textFields, IServiceItemsRepository serviceItems) {
+            TextFields = textFields;
+            ServiceItems = serviceItems;
+        }
+    }
+}
