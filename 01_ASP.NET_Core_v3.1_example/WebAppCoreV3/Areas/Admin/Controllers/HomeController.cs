fa73791d4ce100177b710d1d4f16fd53983ec0a9
Vik154 <Smorodin_1209@mail.ru>
2023-07-21 10:03:00 +0700

    Настройка полей, чек-боксов

---
+++ 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppCoreV3.Areas.Admin.Controllers {
+
+    [Area("Admin")]
+    public class HomeController : Controller {
+        
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
