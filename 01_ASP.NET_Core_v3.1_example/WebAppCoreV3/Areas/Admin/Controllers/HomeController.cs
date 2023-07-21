3ced1f1360e64b2dbe5d142dec7447c69d618b42
Vik154 <Smorodin_1209@mail.ru>
2023-07-21 10:08:37 +0700

    Настройка пространства имен

--- 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Areas/Admin/Controllers/HomeController.cs
+++ 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Areas/Admin/Controllers/HomeController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAppCoreV3.Domain;
 
 namespace WebAppCoreV3.Areas.Admin.Controllers {
 
     [Area("Admin")]
     public class HomeController : Controller {
+
+        private readonly DataManager _dataManager;
+        public HomeController(DataManager dataManager) => _dataManager = dataManager;
         
         public IActionResult Index() {
-            return View();
+            return View(_dataManager.ServiceItems.GetServiceItems());
         }
     }
 }
