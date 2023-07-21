368c9fe8d6f3b669c194aa7c69b14ba2e558f369
Vik154 <Smorodin_1209@mail.ru>
2023-07-22 06:27:24 +0700

    Подключение к БД

--- 02_ASP.NET_Core_v3.1_Shop/DataBase/AppDBContent.cs
+++ 02_ASP.NET_Core_v3.1_Shop/DataBase/AppDBContent.cs
@@ -1,10 +1,14 @@
 ﻿// Класс для работы с базой данных
 using Microsoft.EntityFrameworkCore;
+using Shop.Models;
 
 namespace Shop.DataBase {
     
     public class AppDBContent : DbContext {
         public AppDBContent(DbContextOptions<AppDBContent> options) 
             : base(options) { }            
+    
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
