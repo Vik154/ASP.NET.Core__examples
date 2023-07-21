2672536e4150ee53d9aa832c09e55eebe522867a
Vik154 <Smorodin_1209@mail.ru>
2023-07-21 12:08:49 +0700

    release SideBar

---
+++ 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebAppCoreV3.Domain;
+
+namespace WebAppCoreV3.Models.ViewComponents {
+
+    public class SidebarViewComponent : ViewComponent {
+        private readonly DataManager dataManager;
+
+        public SidebarViewComponent(DataManager dataManager) {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync() {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.ServiceItems.GetServiceItems()));
+        }
+    }
+}
