8df237d20914f8ea49387a6b7929680af5dd7b9a
Vik154 <Smorodin_1209@mail.ru>
2023-07-21 11:08:21 +0700

    Controller implement

--- 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Controllers/HomeController.cs
+++ 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Controllers/HomeController.cs
@@ -1,10 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAppCoreV3.Domain;
 
 namespace WebAppCoreV3.Controllers {
 
     public class HomeController : Controller {
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager) {
+            this.dataManager = dataManager;
+        }
+
         public IActionResult Index() {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+
+        public IActionResult Contacts() {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }
