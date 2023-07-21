88deb42f1c02ce0f65e2c7657b3c64c1fd27bf52
Vik154 <Smorodin_1209@mail.ru>
2023-07-21 10:42:10 +0700

    Edit implemet

---
+++ 01_ASP.NET_Core_v3.1_example/WebAppCoreV3/Areas/Admin/Controllers/ServiceItemsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppCoreV3.Areas.Admin.Controllers {
+    public class ServiceItemsController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
