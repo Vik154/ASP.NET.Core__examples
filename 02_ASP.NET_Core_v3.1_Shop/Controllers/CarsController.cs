221f29c44cf555c8f3de95270d7310cf2c743778
Vik154 <Smorodin_1209@mail.ru>
2023-07-22 03:42:39 +0700

    Создание и реализация контроллера

---
+++ 02_ASP.NET_Core_v3.1_Shop/Controllers/CarsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Interfaces;
+
+namespace Shop.Controllers {
+   
+    public class CarsController : Controller {
+
+        private readonly IAllCars _allCars;
+        private readonly ICarsCategory _carsCategory;
+
+        public CarsController(IAllCars allCars, ICarsCategory carsCategory) { 
+            _allCars = allCars;
+            _carsCategory = carsCategory;
+        }
+
+        // Результат возвращается в виде html странички
+        public ViewResult List() {
+            var cars = _allCars.Cars;
+            return View(cars);
+        } 
+    }
+}
