d5ca877bfb6705f27e3cbcfbfd1ee141382c28c4
Vik154 <Smorodin_1209@mail.ru>
2023-07-22 07:23:12 +0700

    Добавление миграции

---
+++ 02_ASP.NET_Core_v3.1_Shop/Migrations/20230722002257_Initial.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _02_ASP.NET_Core_v3._1_Shop.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    categoryName = table.Column<string>(nullable: true),
+                    desc = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    shortDesc = table.Column<string>(nullable: true),
+                    longDesc = table.Column<string>(nullable: true),
+                    img = table.Column<string>(nullable: true),
+                    price = table.Column<int>(nullable: false),
+                    isFavourite = table.Column<bool>(nullable: false),
+                    available = table.Column<bool>(nullable: false),
+                    categoryID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cars_Categories_categoryID",
+                        column: x => x.categoryID,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_categoryID",
+                table: "Cars",
+                column: "categoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
