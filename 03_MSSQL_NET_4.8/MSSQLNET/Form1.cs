92c6343b330493a21607847eb65c04af1a17df85
Vik154 <Smorodin_1209@mail.ru>
2023-07-22 15:45:48 +0700

    Подключение к БД

--- 03_MSSQL_NET_4.8/MSSQLNET/Form1.cs
+++ 03_MSSQL_NET_4.8/MSSQLNET/Form1.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace MSSQLNET {
     public partial class Form1 : Form {
+
+        // Подключение к БД
+        private SqlConnection sqlConnection = null;
+
         public Form1() {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // При загрузке формы подключаемся к БД
+            sqlConnection = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString);
         }
     }
 }
