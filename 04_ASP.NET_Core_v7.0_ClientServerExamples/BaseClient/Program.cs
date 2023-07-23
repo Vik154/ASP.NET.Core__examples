06c56f0d978fb8bd85a788b3d00cb4e8c38a6aff
Vik154 <Smorodin_1209@mail.ru>
2023-07-23 12:24:42 +0700

    Пример работы GetFromJsonAsync и DeleteFromJsonAsync

--- 04_ASP.NET_Core_v7.0_ClientServerExamples/BaseClient/Program.cs
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/BaseClient/Program.cs
@@ -1,7 +1,35 @@
-﻿namespace BaseClient {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+﻿// Пример простого клиента (работа с запросами/протоколами)
+using System.Net.Http.Json; // пространство имен метода GetFromJsonAsync
+
+namespace BaseClient;
+
+
+class Program {
+
+    static HttpClient httpClient = new HttpClient();
+    
+    static async Task Main() {
+        // Прмер 1, работа метода GetFromJsonAsync()
+        await ExampleGetFromJsonAsync();
+
+        // Пример 2, работа DeleteFromJsonAsync()
+        await ExampleDeleteFromJsonAsync();
+    }
+
+    // EX-1.0 GetFromJsonAsync() - отправляет запрос GET и возвращает десериализованные объекты из JSON
+    static async Task ExampleGetFromJsonAsync() {
+        object? data = await httpClient.GetFromJsonAsync("https://localhost:7219/", typeof(Person));
+        if (data is Person person) {
+            Console.WriteLine($"Name: {person.Name}   Age: {person.Age}");
         }
     }
+
+    // EX-2.0 DeleteFromJsonAsync() - отправляет запрос DELETE и возвращает десериализованные объекты из JSON
+    static async Task ExampleDeleteFromJsonAsync() {
+        Person? person = await httpClient.GetFromJsonAsync<Person>("https://localhost:7219/");
+        Console.WriteLine($"Name: {person?.Name}   Age: {person?.Age}");
+    }
+
 }
+
+record Person(string Name, int Age);
