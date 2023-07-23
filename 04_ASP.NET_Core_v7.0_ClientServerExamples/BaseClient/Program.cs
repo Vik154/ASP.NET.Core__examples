3ce0ef954f17d230367a3e27e54e5b3aba0e433f
Vik154 <Smorodin_1209@mail.ru>
2023-07-23 12:45:19 +0700

    Получение данных с сервера в формате JSON

--- 04_ASP.NET_Core_v7.0_ClientServerExamples/BaseClient/Program.cs
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/BaseClient/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Пример простого клиента (работа с запросами/протоколами)
+using System.Net;
 using System.Net.Http.Json; // пространство имен метода GetFromJsonAsync
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BaseClient;
 
@@ -10,10 +12,12 @@ class Program {
     
     static async Task Main() {
         // Прмер 1, работа метода GetFromJsonAsync()
-        await ExampleGetFromJsonAsync();
+        // await ExampleGetFromJsonAsync();
 
         // Пример 2, работа DeleteFromJsonAsync()
-        await ExampleDeleteFromJsonAsync();
+        // await ExampleDeleteFromJsonAsync();
+
+        await Example_2();
     }
 
     // EX-1.0 GetFromJsonAsync() - отправляет запрос GET и возвращает десериализованные объекты из JSON
@@ -30,6 +34,26 @@ class Program {
         Console.WriteLine($"Name: {person?.Name}   Age: {person?.Age}");
     }
 
+    // Пример 2 (На сервере)
+    static async Task Example_2() {
+        using var response = await httpClient.GetAsync("https://localhost:7219/1");
+
+        if (response.StatusCode == HttpStatusCode.BadRequest || response.StatusCode == HttpStatusCode.NotFound) {
+            // получаем информацию об ошибке
+            Error? error = await response.Content.ReadFromJsonAsync<Error>();
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(error?.Message);
+        }
+        else {
+            // если запрос завершился успешно, получаем объект Person
+            Person? person = await response.Content.ReadFromJsonAsync<Person>();
+            Console.WriteLine($"Name: {person?.Name}   Age: {person?.Age}");
+        }
+    }
+
 }
 
+// для успешного ответа
 record Person(string Name, int Age);
+// для ошибок
+record Error(string Message);
--- 04_ASP.NET_Core_v7.0_ClientServerExamples/BaseServer/Program.cs
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/BaseServer/Program.cs
@@ -6,6 +6,13 @@ namespace BaseServer;
 
 public class Program {
     public static void Main(string[] args) {
+        // Пример 1        Example_1();
+        // Пример 2
+        Example_2();
+    }
+
+    // Пример 1
+    static void Example_1() {
         // сначала необходимо создать объект WebApplicationBuilder:
         var builder = WebApplication.CreateBuilder();
         // с помощью Build создаем объект WebApplication, который представляет приложение ASP.NET:
@@ -13,6 +20,24 @@ public class Program {
         // С помощью app.MapGet() определяем конечную точку - обработчик запросов по определенному маршруту:
         app.MapGet("/", () => new Person("Tom", 38));
         // Затем запускаем приложение:
+        app.Run();
+    }
+
+    // Пример 2
+    static void Example_2() {
+        var builder = WebApplication.CreateBuilder();
+        var app = builder.Build();
+
+        app.MapGet("/{id?}", (int? id) =>
+        {
+            if (id is null)
+                return Results.BadRequest(new { Message = "Некорректные данные в запросе" });
+            else if (id != 1)
+                return Results.NotFound(new { Message = $"Объект с id={id} не существует" });
+            else
+                return Results.Json(new Person("Bob", 42));
+        });
+
         app.Run();
     }
 }
