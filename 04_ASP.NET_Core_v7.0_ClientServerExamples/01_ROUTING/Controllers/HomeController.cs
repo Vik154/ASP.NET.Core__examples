c4a870b533f76e10083509ed27f37f094bbfc8b3
Vik154 <Smorodin_1209@mail.ru>
2023-07-29 11:31:42 +0700

    Пример взаимодействия контроллера с представлением

---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/01_ROUTING/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Routing.Controllers {
+
+    public class HomeController : Controller {
+
+        // GET: HomeController; View: Idex
+        public ActionResult Index() {
+            return View();
+        }
+
+        // View: List
+        public IActionResult List() {
+            string[] list = { "item 1", "item 2", "item 3" };
+            return View(list);
+        }
+
+        // Пример маршрута с параметрами. Запрос в строке браузера:
+        // https://localhost:7253/home/sumres/2/5
+        public int SumRes(int x, int y) {
+            return x + y;
+        }
+
+        // GET: HomeController/Details/5
+        public ActionResult Details(int id) {
+            return View();
+        }
+
+        // GET: HomeController/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: HomeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Edit/5
+        public ActionResult Edit(int id) {
+            return View();
+        }
+
+        // POST: HomeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Delete/5
+        public ActionResult Delete(int id) {
+            return View();
+        }
+
+        // POST: HomeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+    }
+}
--- 04_ASP.NET_Core_v7.0_ClientServerExamples/01_ROUTING/Program.cs
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/01_ROUTING/Program.cs
@@ -1,12 +1,23 @@
-namespace _01_ROUTING {
-    public class Program {
-        public static void Main(string[] args) {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+namespace Routing;
 
-            app.MapGet("/", () => "Hello World!");
 
-            app.Run();
-        }
+public class Program {
+    public static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+        // builder.Services.AddControllers();        // добавляем поддержку контроллеров
+        builder.Services.AddControllersWithViews();  // добавляем поддержку контроллеров с представлениями
+        var app = builder.Build();
+
+        // Специфичные маршруты лучше расположить в начале
+        app.MapControllerRoute(
+            name: "TwoParametrs",
+            pattern: "{controller}/{action}/{x}/{y}");
+        // устанавливаем сопоставление маршрутов с контроллерами
+        // Более общий маршрут распологают в конце списка
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
 }
