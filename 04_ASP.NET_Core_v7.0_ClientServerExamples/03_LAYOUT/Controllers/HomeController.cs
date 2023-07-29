1abd84a2919375f9c9ec2bdc1609705dc50eb5d3
Vik154 <Smorodin_1209@mail.ru>
2023-07-29 20:53:59 +0700

    Строго типизированные частичные представления

--- 04_ASP.NET_Core_v7.0_ClientServerExamples/03_LAYOUT/Controllers/HomeController.cs
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/03_LAYOUT/Controllers/HomeController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LtModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Layout.Controllers;
 
 public class HomeController : Controller {
-    public IActionResult Index()    => View();
+    public IActionResult Index()    => View(model);
     public IActionResult Catalog()  => View();
-    public IActionResult Contacts() => View();
+    public IActionResult Contacts() => View(model);
+
+    ContactsViewModel model;
+    public HomeController() {
+        model = new ContactsViewModel() {
+            Email = "user@mail.com",
+            Phone = "+9 999 8888 99 98",
+            Address = "Str. abcd 20"
+        };
+    }
 }
---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/03_LAYOUT/Model/ContactsViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace LtModel;
+
+public class ContactsViewModel {
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+}
