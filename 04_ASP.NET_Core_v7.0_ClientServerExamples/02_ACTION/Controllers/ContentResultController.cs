45a52966475da7e7f42527e1dd2da910daa937fc
Vik154 <Smorodin_1209@mail.ru>
2023-07-29 17:36:44 +0700

    Информирование о возвращаемомо типе контента

---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/02_ACTION/Controllers/ContentResultController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Action.Controllers;
+
+public class ContentResultController : Controller {
+
+    public IActionResult Index()   => View();
+    public IActionResult Sample1() => Content("Hello world", "text/plain");   // Ответ - Как простой текст
+    public IActionResult Sample2() => Content("[1,2,3]", "application/json"); // Ответ в виде JSON контента
+}
