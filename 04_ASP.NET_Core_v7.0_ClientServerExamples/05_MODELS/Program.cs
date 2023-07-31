6f0cc8d624a00751ecbc82e981b0a3c27a16d5eb
Vik154 <Smorodin_1209@mail.ru>
2023-07-31 20:25:46 +0700

    Добавление провайдера в коллекцию провайдеров привязчиков модели.

--- 04_ASP.NET_Core_v7.0_ClientServerExamples/05_MODELS/Program.cs
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/05_MODELS/Program.cs
@@ -1,9 +1,16 @@
+using _05_MODELS.Infrastructure;
+
 namespace _05_MODELS;
 
 public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddControllersWithViews();
+
+        // добавление провайдера CustomDateTime в коллекцию провайдеров привязчиков модели.
+        builder.Services.AddControllersWithViews(opts => {
+            opts.ModelBinderProviders.Insert(0, new CustomDateTimeModelBinderProvider());
+        });
+
         var app = builder.Build();
 
         app.MapControllerRoute(name: "default", pattern: "{controller=Event}/{action=Create}/{id?}");
