af6c3cce53ab88cad08e559a3caff35314b248ec
Vik154 <Smorodin_1209@mail.ru>
2023-07-31 20:22:46 +0700

    Реализация провайдера привязчика модели.

---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/05_MODELS/Infrastructure/CustomDateTimeModelBinderProvider.cs
@@ -0,0 +1,17 @@
+﻿// провайдер привязчика модели
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+namespace _05_MODELS.Infrastructure;
+
+public class CustomDateTimeModelBinderProvider : IModelBinderProvider {
+
+    // GetBinder() - с помощью контекста провайдера ModelBinderProviderContext
+    // мы можем получить тип данных, для которых выполняется привязка
+    public IModelBinder? GetBinder(ModelBinderProviderContext context) {
+
+        // Для объекта SimpleTypeModelBinder необходим сервис ILoggerFactory Получаем его из сервисов
+        ILoggerFactory loggerFactory = context.Services.GetRequiredService<ILoggerFactory>();
+        IModelBinder binder = new CustomDateTimeModelBinder(new SimpleTypeModelBinder(typeof(DateTime), loggerFactory));
+        return context.Metadata.ModelType == typeof(DateTime) ? binder : null;
+    }
+}
