a4167f4544dcbb2ba05037294aa6707017056c96
Vik154 <Smorodin_1209@mail.ru>
2023-07-31 19:11:45 +0700

    Пример работы с моделями

---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/05_MODELS/Program.cs
@@ -0,0 +1,14 @@
+namespace _05_MODELS;
+
+
+public class Program {
+    public static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews();
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
