1fe22e9cb3e0f7f7809a6754427579d04c2309f3
Vik154 <Smorodin_1209@mail.ru>
2023-08-01 12:22:05 +0700

    Генерация ссылок и маршрутов с помощью tag - хелпера

--- 04_ASP.NET_Core_v7.0_ClientServerExamples/07_TAGHelpers/Program.cs
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/07_TAGHelpers/Program.cs
@@ -7,7 +7,7 @@ public class Program {
         builder.Services.AddControllersWithViews();
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.MapControllerRoute(name: "Default", pattern: "{controller=Home}/{action=Index}");
 
         app.Run();
     }
