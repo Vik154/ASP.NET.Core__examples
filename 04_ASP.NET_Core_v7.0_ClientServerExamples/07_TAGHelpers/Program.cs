6ed978bd6c2071c0361d6a87ccccfe421e59e623
Vik154 <Smorodin_1209@mail.ru>
2023-08-01 11:56:58 +0700

    Примеры работы tag - хелперов

---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/07_TAGHelpers/Program.cs
@@ -0,0 +1,14 @@
+namespace _07_TAGHelpers;
+
+
+public class Program {
+    public static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews();
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
