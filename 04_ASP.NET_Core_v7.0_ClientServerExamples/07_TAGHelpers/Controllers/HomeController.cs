e3fbbef8c55111801f402787da02bac0aeb15384
Vik154 <Smorodin_1209@mail.ru>
2023-08-01 12:05:18 +0700

    Подключение tag-хелперов во все представления в категории

---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/07_TAGHelpers/Controllers/HomeController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace _07_TAGHelpers.Controllers;
+
+public class HomeController : Controller {
+
+    public IActionResult Index() => View();
+    public IActionResult About() => View();
+}
