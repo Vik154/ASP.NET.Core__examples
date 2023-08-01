8e9f14009b5b1139b4bcbe8c9dd75194a8f544f5
Vik154 <Smorodin_1209@mail.ru>
2023-08-01 16:36:38 +0700

    Примеры компонентов представлений

---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/08_VIEW_COMPONENTS/Program.cs
@@ -0,0 +1,13 @@
+namespace _08_VIEW_COMPONENTS;
+
+public class Program {
+    public static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews();
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
