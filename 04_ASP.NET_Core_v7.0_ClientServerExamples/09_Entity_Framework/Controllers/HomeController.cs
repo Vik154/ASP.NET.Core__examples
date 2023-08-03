7c234cdaac2dcbc69e794a54d0e84a0cfde9b26b
Vik154 <Smorodin_1209@mail.ru>
2023-08-03 17:26:25 +0700

    Получение контекста данных в контроллере

---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/09_Entity_Framework/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿// Стандартный контроллер
+// Получение контекста данных в контроллере
+using _09_Entity_Framework.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _09_Entity_Framework.Controllers;
+
+
+public class HomeController : Controller {
+
+    // Для взаимодействия с базой данных определяется переменная ApplicationContext db.
+    ApplicationContext db;
+
+    // получить переданный контекст данных
+    public HomeController(ApplicationContext context) => db = context;
+
+    // Получение объектов из бд, создание из них списка и передача в представление.
+    public async Task<IActionResult> Index() => View(await db.Users.ToListAsync());
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> Create(User user) {
+        db.Users.Add(user);                 // для данных из объекта user формируется sql-выражение INSERT
+        await db.SaveChangesAsync();        // выполняет это выражение, тем самым добавляя данные в базу данных
+        return RedirectToAction("Index");
+    }
+}
