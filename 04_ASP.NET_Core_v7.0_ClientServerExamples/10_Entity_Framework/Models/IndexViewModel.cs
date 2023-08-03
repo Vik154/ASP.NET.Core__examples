d1f2dbfb690165422ad04359a777b4d708e38dfa
Vik154 <Smorodin_1209@mail.ru>
2023-08-03 19:39:21 +0700

    Класс для представления в Index модели

---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/10_Entity_Framework/Models/IndexViewModel.cs
@@ -0,0 +1,7 @@
+﻿// Класс для представления в Index модели
+namespace _10_Entity_Framework.Models; 
+
+public class IndexViewModel {
+    public IEnumerable<User> Users { get; set; } = new List<User>();
+    public SortViewModel SortViewModel { get; set; } = new SortViewModel(SortState.NameAsc);
+}
