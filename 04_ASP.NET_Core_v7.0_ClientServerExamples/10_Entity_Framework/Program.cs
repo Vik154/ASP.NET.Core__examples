2047f13caf531e173ceff887fdf5032e7582a5f0
Vik154 <Smorodin_1209@mail.ru>
2023-08-03 19:08:16 +0700

    Настройка проекта

--- 04_ASP.NET_Core_v7.0_ClientServerExamples/10_Entity_Framework/Program.cs
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/10_Entity_Framework/Program.cs
@@ -1,12 +1,16 @@
-namespace _10_Entity_Framework {
-    public class Program {
-        public static void Main(string[] args) {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+using _10_Entity_Framework.Models;
+using Microsoft.EntityFrameworkCore;
 
-            app.MapGet("/", () => "Hello World!");
+namespace _10_Entity_Framework;
 
-            app.Run();
-        }
+public class Program {
+    public static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+        string connect = "Server = (localdb)\\mssqllocaldb;Database = userstoredb;Trusted_Connection=true";
+        builder.Services.AddDbContext<UsersContext>(opt => opt.UseSqlServer(connect));
+        builder.Services.AddControllersWithViews();
+        var app = builder.Build();
+        app.MapDefaultControllerRoute();
+        app.Run();
     }
 }
