491b9d74461174308cdcaf4e752c937d1632c985
Vik154 <Smorodin_1209@mail.ru>
2023-08-03 19:02:05 +0700

    Создание моделей

---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/10_Entity_Framework/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace _10_Entity_Framework.Models;
+
+public class User {
+    public int Id { get; set; }
+    public int Age { get; set; }
+    public int? CompanyId { get; set; }
+    public string? Name { get; set; }
+    public Company? company { get; set; }
+}
