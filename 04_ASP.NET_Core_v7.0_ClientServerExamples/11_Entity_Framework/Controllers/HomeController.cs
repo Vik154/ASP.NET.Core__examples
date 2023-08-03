80fdc9827660382f578cc5462234e6c782da4430
Vik154 <Smorodin_1209@mail.ru>
2023-08-03 19:55:43 +0700

    Представление данных модели UserList

---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/11_Entity_Framework/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _11_Entity_Framework.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
