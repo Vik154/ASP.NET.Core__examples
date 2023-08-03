f6603bdf0c2bec62e618cb8b29eeafc14a3f74ef
Vik154 <Smorodin_1209@mail.ru>
2023-08-03 17:17:46 +0700

    Настройки проекта, получение строки подключения, добавление контекста БД

--- 04_ASP.NET_Core_v7.0_ClientServerExamples/09_Entity_Framework/Program.cs
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/09_Entity_Framework/Program.cs
@@ -1,13 +1,23 @@
 // Работа с данными в Entity Framework
+using _09_Entity_Framework.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace _09_Entity_Framework;
 
 public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
+
+        // Получение строки подключения к БД
+        string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+        
+        // добавляем контекст ApplicationContext в качестве сервиса в приложение
+        builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+
+        builder.Services.AddControllersWithViews();
+
         var app = builder.Build();
-
-        app.MapGet("/", () => "Hello World!");
-
+        app.MapDefaultControllerRoute();
         app.Run();
     }
 }
