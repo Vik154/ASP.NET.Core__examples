9c8ed31538add5a41fd5a62aff647ef9826e87f9
Vik154 <Smorodin_1209@mail.ru>
2023-08-03 19:53:41 +0700

    Фильтрация по некоторым критериям

---
+++ 04_ASP.NET_Core_v7.0_ClientServerExamples/11_Entity_Framework/Models/UserListViewModel.cs
@@ -0,0 +1,9 @@
+﻿// Фильтрация по некоторым критериям
+using Microsoft.AspNetCore.Mvc.Rendering;
+namespace _11_Entity_Framework.Models;
+
+public class UserListViewModel {
+    public IEnumerable<User> Users { get; set; } = new List<User>();
+    public SelectList Companies { get; set; } = new SelectList(new List<Company>(), "Id", "Name");
+    public string? Name { get; set; }
+}
