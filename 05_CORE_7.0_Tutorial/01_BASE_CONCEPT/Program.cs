0c5b9d9b85e429891360ba45151c393939a67ed5
Vik154 <Smorodin_1209@mail.ru>
2023-08-05 18:13:47 +0700

    USE -- Передача обработки запроса следующим компонетам конвеера middleware

--- 05_CORE_7.0_Tutorial/01_BASE_CONCEPT/Program.cs
+++ 05_CORE_7.0_Tutorial/01_BASE_CONCEPT/Program.cs
@@ -1,5 +1,4 @@
 using _01_BASE_CONCEPT.Services;
-
 namespace _01_BASE_CONCEPT;
 
 public class Program {
@@ -7,8 +6,36 @@ public class Program {
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
-        app.Run(SenderForm.SendHtmlForm);
+        // Use - Передача обработки запроса следующим компонетам конвеера middleware
+        app.Use(_01_MiddlewareUse.Step3);
+        app.Run(_01_MiddlewareUse.Step4);
+        // app.Use(_01_MiddlewareUse.Step1);
+        // app.Run(_01_MiddlewareUse.Step2);
+
+        // 01 - Отправка форм
+        // app.Run(SenderForm.SendHtmlForm);
 
         app.Run();
     }
 }
+
+/*
+ * 1 - USE:
+ 
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+ 
+string date = "";
+ 
+app.Use(async(context, next) => {
+    date = DateTime.Now.ToShortDateString();     // Действия до передачи запроса дальше по конвееру 
+    await next.Invoke();                         // вызываем middleware из app.Run
+    Console.WriteLine($"Current date: {date}");  // Действия после обработки запроса следующим middleware
+});
+ 
+// Следующий middleware
+app.Run(async(context) => await context.Response.WriteAsync($"Date: {date}"));
+ 
+app.Run();
+ 
+ */
---
+++ 05_CORE_7.0_Tutorial/01_BASE_CONCEPT/Services/01_MiddlewareUse.cs
@@ -0,0 +1,39 @@
+﻿// Пример работы метода IApplicationBuilder Use
+// Передача обработки запроса следующим компонетам конвеера middleware
+namespace _01_BASE_CONCEPT.Services;
+
+// Метод расширения Use() имеет следующие версии:
+// public static IApplicationBuilder Use(this IApplicationBuilder app, Func<HttpContext, Func<Task>, Task> middleware);
+// public static IApplicationBuilder Use(this IApplicationBuilder app, Func<HttpContext, RequestDelegate, Task> middleware)
+public class _01_MiddlewareUse {
+
+    private static string Date { get; set; } = "";
+
+    // Передается в метод Use
+    public static async Task Step1(HttpContext context, Func<Task> next) {
+        Date = DateTime.Now.ToShortDateString();    // действия перед передачей запроса в следующий middleware
+        await next.Invoke();                        // Передача запроса дальше по конвееру в app.Run
+        Console.WriteLine($"Текущая дата: {Date}"); // действия после обработки запроса следующим middleware
+        Console.WriteLine($"Context: {context.Request.Path.Value}");
+    }
+
+    // Передатся в метод Run
+    public static async Task Step2(HttpContext context) {
+        await context.Response.WriteAsync($"Date: {Date}");
+    }
+
+    // Использование метода Use как терминального компонента middleware
+    public static async Task Step3(HttpContext context, Func<Task> next) {
+
+        string? path = context.Request.Path.Value?.ToLower();
+
+        if (path == "/date")
+            await context.Response.WriteAsync($"Date: {DateTime.Now.ToShortDateString()}");
+        else
+            await next.Invoke();
+    }
+
+    public static async Task Step4(HttpContext context) {
+        await context.Response.WriteAsync("Hello World");
+    }
+}
