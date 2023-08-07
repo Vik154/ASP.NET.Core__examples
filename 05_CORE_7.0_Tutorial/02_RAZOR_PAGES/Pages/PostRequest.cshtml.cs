6539d9f85cc74c081bc662071ca7526ab90b1550
Vik154 <Smorodin_1209@mail.ru>
2023-08-07 12:21:41 +0700

    Get - Запросы

---
+++ 05_CORE_7.0_Tutorial/02_RAZOR_PAGES/Pages/PostRequest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _02_RAZOR_PAGES.Pages
+{
+    public class PostRequestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
